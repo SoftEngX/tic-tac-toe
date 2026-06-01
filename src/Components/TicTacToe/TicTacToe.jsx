dba9c2f79e1ec19735b265c2f9f1c8227e22ee06
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2026-06-01 23:24:46 +0600

    Add styles for title

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './TicTacToe.css'
-import circle from '../../assets/circle.png'
-import cross from '../../assets/cross.png'
+import circle from '../assets/circle.png'
+import cross from '../assets/cross.png'
 
 export default function TicTacToe() {
   return (
-    <div>TicTacToe</div>
+    <div className='container'>
+      <h1 className='title'>Tic Tac Toe Game In <span>React</span></h1>
+
+      <div className="board">
+
+      </div>
+      <button className="reset">Reset</button>
+    </div>
   )
 }
