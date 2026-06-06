d5430b25d4b45bf15516799c86d37dff98c066d5
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2026-06-06 07:18:21 +0600

    Add rows boxes

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -5,13 +5,28 @@ import cross from '../assets/cross.png'
 
 export default function TicTacToe() {
   return (
-    <div className='container'>
-      <h1 className='title'>Tic Tac Toe Game In <span>React</span></h1>
-
+    <div className="container">
+      <h1 className="title">
+        Tic Tac Toe Game In <span>React</span>
+      </h1>
       <div className="board">
-
+        <div className="row1">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row2">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row3">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
       </div>
       <button className="reset">Reset</button>
     </div>
-  )
+  );
 }
